c26509a27cb2a26760aab25f6f4841b27cdd0c26
Gary Becks <techstar.dev@hotmail.com>
2020-10-09 13:15:20 -0400

    add json support to schema

--- internal/schema.go
+++ internal/schema.go
@@ -23,56 +23,71 @@ func (t TypeKind) String() string {
 		return "bool"
 	case TypeInt:
 		return "int"
-	cas
+	case TypeFloat:
+		return "float"
+	case TypeString:
+		return "string"
+	case TypeObject:
+		return "object"
+	case TypeArray:
+		return "array"
+	case TypeMaybe:
+		return "maybe"
+	case TypeStruct:
+		return "struct"
+	case TypeEnum:
+		return "enum"
+	case TypeAlias:
+		return "alias"
 	}
-
+	return ""
 }
 
 // Type represents a varlink type. Types are method input and output parameters,
 // error output parameters, or custom defined types in the interface description.
 type Type struct {
-	Kind        TypeKind
-	ElementType *Type
-	Alias       string
-	Fields      []TypeField
+	Kind        TypeKind    `json:"kind"`
+	ElementType *Type       `json:"type"`
+	Alias       string      `json:"alias"`
+	Fields      []TypeField `json:"fields"`
 }
 
 // TypeField is a named member of a TypeStruct.
 type TypeField struct {
-	Name string
-	Type *Type
+	Name string `json:"name"`
+	Type *Type  `json:"type"`
 }
 
 // Alias represents a named Type in the interface description.
 type Alias struct {
-	Name string
-	Doc  string
-	Type *Type
+	Name string `json:"name"`
+	Doc  string `json:"doc"`
+	Type *Type  `json:"type"`
 }
 
 // Method represents a method defined in the interface description.
 type Method struct {
-	Name string
-	Doc  string
-	In   *Type
-	Out  *Type
+	Name string `json:"name"`
+	Doc  string `json:"doc"`
+	In   *Type  `json:"in"`
+	Out  *Type  `json:"out"`
 }
 
 // Error represents an error defined in the interface description.
 type Error struct {
-	Name string
-	Doc  string
-	Type *Type
+	Name string `json:"name"`
+	Doc  string `json:"doc"`
+	Type *Type  `json:"type"`
 }
 
 // IDL represents a parsed varlink interface description with types, methods, errors and
 // documentation.
 type IDL struct {
-	Name        string
-	Doc         string
-	Description string
-	Members     []interface{}
-	Aliases     []*Alias
-	Methods     []*Method
-	Errors      []*Error
+	Name        string        `json:"name"`
+	Doc         string        `json:"doc"`
+	Description string        `json:"description"`
+	Members     []interface{} `json:"members"`
+	Aliases     []*Alias      `json:"aliases"`
+	Methods     []*Method     `json:"methods"`
+	Errors      []*Error      `json:"errors"`
 }
